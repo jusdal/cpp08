ce078fe990d347f8d29503a5d00ad1345cce3f88
jusdal <justin.m.daly@gmail.com>
2024-06-28 18:13:57 +0700

    add main test from intra

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,24 @@
 /*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:03:47 by justindaly        #+#    #+#             */
-/*   Updated: 2024/06/28 18:03:48 by justindaly       ###   ########.fr       */
+/*   Updated: 2024/06/28 18:13:48 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+int main()
+{
+    Span sp = Span(5);
+
+    sp.addNumber(6);
+    sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+	return 0;
+}
