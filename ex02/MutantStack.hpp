4950edb95f042fea0626cb1e84a6c7fe6820e822
Justin Daly <justin.m.daly@gmail.com>
2024-08-16 22:46:44 +0700

    rewrite Mutant stack more simply

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 20:11:13 by justindaly        #+#    #+#             */
-/*   Updated: 2024/06/28 20:23:35 by justindaly       ###   ########.fr       */
+/*   Updated: 2024/08/16 21:54:10 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,53 +21,23 @@ template < typename T, typename C = std::deque<T> >
 class MutantStack : public std::stack<T, C>
 {
 	public:
-		// Typedefs for iterators
-		typedef typename std::stack<T, C>::container_type::iterator					iterator;
-		typedef typename std::stack<T, C>::container_type::const_iterator			const_iterator;
-		typedef typename std::stack<T, C>::container_type::reverse_iterator			reverse_iterator;
-		typedef typename std::stack<T, C>::container_type::const_reverse_iterator	const_reverse_iterator;
 
-		MutantStack<T, C>(void) : std::stack<T, C>() {};
-		MutantStack<T, C>(MutantStack<T, C> const & src) : std::stack<T, C>(src) {};
-		~MutantStack<T, C>(void) {};
+		MutantStack() {};
+		MutantStack(MutantStack const & src) { *this = src; };
+		~MutantStack() {};
 
-		MutantStack<T, C> &	operator=(MutantStack<T, C> const & src) {
+		MutantStack&	operator=(MutantStack const & src)
+		{
 			if (this != &src)
 				this->c = src.c;
 			return (*this);
 		}
 
-		MutantStack<T, C>::iterator					begin(void) {
-			return (this->c.begin());
-		}
+		//Iterator Definitions
+		typedef typename C::iterator	iterator;
 
-		MutantStack<T, C>::iterator					end(void) {
-			return (this->c.end());
-		}
-
-		MutantStack<T, C>::const_iterator			begin(void) const {
-			return (this->c.begin());
-		}
-
-		MutantStack<T, C>::const_iterator			end(void) const {
-			return (this->c.end());
-		}
-
-		MutantStack<T, C>::reverse_iterator			rbegin(void) {
-			return (this->c.rbegin());
-		}
-
-		MutantStack<T, C>::reverse_iterator			rend(void) {
-			return (this->c.rend());
-		}
-
-		MutantStack<T, C>::const_reverse_iterator	rbegin(void) const {
-			return (this->c.rbegin());
-		}
-
-		MutantStack<T, C>::const_reverse_iterator	rend(void) const {
-			return (this->c.rend());
-		}
+		iterator	begin() { return this->c.begin(); }
+		iterator	end() { return this->c.end(); }
     };
 
 #endif // MUTANTSTACK_HPP
